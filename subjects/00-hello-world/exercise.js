800a2a00661ecbd121414cb47f79c10208162233
George Toothman <george.toothman@flatironschool.com>
2015-11-14 14:44:49 -0500

    add state and toggle

--- subjects/00-hello-world/exercise.js
+++ subjects/00-hello-world/exercise.js
@@ -2,9 +2,19 @@ import React from 'react'
 import { render } from 'react-dom'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { myHeading: 'Hello React',
+    toggle: false};
+  }
 
   render() {
-    return <h1>Hello World!</h1>
+    return (
+      <div>
+      <h1>Hello World!</h1>
+      <h2>{this.state.toggle ? this.state.myHeading : 'Nothing'}</h2>
+      </div>
+    )
   }
 
 }
